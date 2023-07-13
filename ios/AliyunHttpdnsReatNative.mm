2b2ae0301cc277d4de1ce71c8c6b791fed12b21d
瑾笙 <allen.wy@alibaba-inc.com>
2023-07-13 14:54:51 +0800

    feat: ios的代码完成待测试

--- ios/AliyunHttpdnsReatNative.mm
+++ ios/AliyunHttpdnsReatNative.mm
@@ -64,6 +64,7 @@ RCT_REMAP_METHOD(initWithAccountId,
 
     _dnsService = [[HttpDnsService alloc] initWithAccountID:[accountId intValue] secretKey:nil];
     [_dnsService setDelegateForDegradationFilter:self];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(initWithAccountIdAndSk,
@@ -84,7 +85,7 @@ RCT_REMAP_METHOD(initWithAccountIdAndSk,
     
     _dnsService = [[HttpDnsService alloc] initWithAccountID:[accountId intValue] secretKey:nil];
     [_dnsService setDelegateForDegradationFilter:self];
-    
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(getIPv4ForHostAsync,
@@ -97,7 +98,17 @@ RCT_REMAP_METHOD(getIPv4ForHostAsync,
         return;
     }
     
+    if (!host || [host isEqual:@""]) {
+        resolve(@{KEY_CODE:CODE_PARAM_ILLEGAL, KEY_ERROR_MSG: @"host is empty"});
+        return;
+    }
     
+    NSString* ipv4 = [_dnsService getIpByHostAsync:host];
+    if (ipv4) {
+        resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success", KEY_RESULT: ipv4});
+    } else {
+        resolve(@{KEY_CODE:CODE_RESULT_EMPTY, KEY_ERROR_MSG: @"ipv4 result is empty"});
+    }
     
 }
 
@@ -106,6 +117,22 @@ RCT_REMAP_METHOD(getIPv4ListForHostAsync,
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
     
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
+    
+    if (!host || [host isEqual:@""]) {
+        resolve(@{KEY_CODE:CODE_PARAM_ILLEGAL, KEY_ERROR_MSG: @"host is empty"});
+        return;
+    }
+    
+    NSArray* ipv4List = [_dnsService getIpsByHostAsync:host];
+    if (ipv4List && ipv4List.count > 0) {
+        resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success", KEY_RESULT: ipv4List});
+    } else {
+        resolve(@{KEY_CODE:CODE_RESULT_EMPTY, KEY_ERROR_MSG: @"ipv4 list result is empty"});
+    }
 }
 
 RCT_REMAP_METHOD(getIPv6ForHostAsync,
@@ -113,20 +140,75 @@ RCT_REMAP_METHOD(getIPv6ForHostAsync,
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
     
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
+    
+    if (!host || [host isEqual:@""]) {
+        resolve(@{KEY_CODE:CODE_PARAM_ILLEGAL, KEY_ERROR_MSG: @"host is empty"});
+        return;
+    }
+    
+    NSString* ipv6 = [_dnsService getIPv6ByHostAsync:host];
+    if (ipv6) {
+        resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success", KEY_RESULT: ipv6});
+    } else {
+        resolve(@{KEY_CODE:CODE_RESULT_EMPTY, KEY_ERROR_MSG: @"ipv6 result is empty"});
+    }
+    
 }
 
 RCT_REMAP_METHOD(getIPv6ListForHostAsync,
                  v6ListWithHost:(NSString *)host
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
     
+    if (!host || [host isEqual:@""]) {
+        resolve(@{KEY_CODE:CODE_PARAM_ILLEGAL, KEY_ERROR_MSG: @"host is empty"});
+        return;
+    }
+    
+    NSArray* ipv6List = [_dnsService getIPv6sByHostAsync:host];
+    if (ipv6List && ipv6List.count > 0) {
+        resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success", KEY_RESULT: ipv6List});
+    } else {
+        resolve(@{KEY_CODE:CODE_RESULT_EMPTY, KEY_ERROR_MSG: @"ipv6 list result is empty"});
+    }
 }
 
 RCT_REMAP_METHOD(getIPv4IPv6ListForHostAsync,
                  v4v6ListWithHost:(NSString *)host
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
     
+    if (!host || [host isEqual:@""]) {
+        resolve(@{KEY_CODE:CODE_PARAM_ILLEGAL, KEY_ERROR_MSG: @"host is empty"});
+        return;
+    }
+    
+    NSDictionary* v4v6Dict = [_dnsService getIPv4_v6ByHostAsync:host];
+    if (!v4v6Dict || v4v6Dict.count == 0) {
+        resolve(@{KEY_CODE:CODE_RESULT_EMPTY, KEY_ERROR_MSG: @"ipv4 and ipv6 list result is empty"});
+        return;
+    }
+    
+    NSMutableDictionary<NSString*, NSArray*>* v4v6Result = [[NSMutableDictionary alloc] init];
+    
+    NSArray* v4List = [v4v6Dict valueForKey:ALICLOUDHDNS_IPV4];
+    NSArray* v6List = [v4v6Dict valueForKey:ALICLOUDHDNS_IPV6];
+    
+    [v4v6Result setValue:v4List forKey:@"ipv4"];
+    [v4v6Result setValue:v6List forKey:@"ipv6"];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success", KEY_RESULT: v4v6Result});
 }
 
 RCT_REMAP_METHOD(setHttpDnsLogEnabled, logWithEnabled:(BOOL) enabled) {
@@ -137,29 +219,74 @@ RCT_REMAP_METHOD(setPreResolveHosts,
                  preResolveWithHost:(NSArray *)hostList
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
     
+    if (!hostList || hostList.count == 0) {
+        resolve(@{KEY_CODE:CODE_PARAM_ILLEGAL, KEY_ERROR_MSG: @"preResolve host list is empty"});
+        return;
+    }
+    
+    [_dnsService setPreResolveHosts:hostList];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(setPreResolveHostsWithIPType,
                  preResolveIPTypeWithHost:(NSArray *)hostList
+                 preResolveIPTypeWithType:(NSNumber *)requestIpType
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject
                  ) {
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
     
+    if (!hostList || hostList.count == 0) {
+        resolve(@{KEY_CODE:CODE_PARAM_ILLEGAL, KEY_ERROR_MSG: @"preResolve host list is empty"});
+        return;
+    }
+    
+    AlicloudHttpDNS_IPType ipType;
+    if ([requestIpType intValue] == 0) {
+        ipType = AlicloudHttpDNS_IPTypeV4;
+    } else if ([requestIpType intValue] == 1) {
+        ipType = AlicloudHttpDNS_IPTypeV6;
+    } else if ([requestIpType intValue] == 2) {
+        ipType = AlicloudHttpDNS_IPTypeV64;
+    } else {
+        //iOS没有自动，默认v4
+        ipType = AlicloudHttpDNS_IPTypeV4;
+    }
+    
+    [_dnsService setPreResolveHosts:hostList queryIPType:ipType];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(setCachedIPEnabled,
                  cachedIPWithEnabled:(BOOL)enabled
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
-    
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
+    [_dnsService setCachedIPEnabled:enabled];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(setExpiredIPEnabled,
                  expiredIPWithEnabled:(BOOL)enabled
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
-    
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
+    [_dnsService setExpiredIPEnabled:enabled];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(setHTTPSRequestEnabled,
@@ -167,13 +294,30 @@ RCT_REMAP_METHOD(setHTTPSRequestEnabled,
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
     
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
+    [_dnsService setHTTPSRequestEnabled:enabled];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
+    
 }
 
 RCT_REMAP_METHOD(setRegion,
                  withRegion:(NSString *)region
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
+    if (!region) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"region is empty"});
+        return;
+    }
     
+    [_dnsService setRegion:region];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(cleanHostCache,
@@ -181,13 +325,44 @@ RCT_REMAP_METHOD(cleanHostCache,
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
     
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
+    
+    if (!hostList || hostList.count == 0) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"host list is empty"});
+        return;
+    }
+    
+    [_dnsService cleanHostCache:hostList];
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(setIPRanking,
                  withIPRankList:(NSArray *)ipRankingList
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject) {
+    if (!_dnsService) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"please call init method first"});
+        return;
+    }
     
+    if (!ipRankingList || ipRankingList.count == 0) {
+        resolve(@{KEY_CODE:CODE_INIT_FIRST, KEY_ERROR_MSG: @"ipRanking list is empty"});
+        return;
+    }
+    
+    NSMutableDictionary* dataSource = [[NSMutableDictionary alloc]init];
+    for(NSDictionary *dict in ipRankingList) {
+        NSString* host = [dict valueForKey:@"hostName"];
+        NSNumber* port = [dict valueForKey:@"port"];
+        [dataSource setValue:port forKey:host];
+    }
+    
+    [_dnsService setIPRankingDatasource:dataSource];
+    
+    resolve(@{KEY_CODE:CODE_SUCCESS, KEY_ERROR_MSG: @"success"});
 }
 
 RCT_REMAP_METHOD(currentIPStack,
